c36afefbab1ddb09fcec9fd16ec34243d34b5f83
lxy20210816 <lxy20210816@gmail.com>
2025-09-04 00:39:35 +0900

    添加邮件发送功能，发送成功了

--- configs/config.py
+++ configs/config.py
@@ -4,3 +4,11 @@
 
 mysql_user = "root"
 mysql_password = "123456"
+
+mail_config = {
+    "username": "lxy20210816@gmail.com",
+    "password": "tdzjekthxiyurpcm",
+    "smtp_server": "smtp.gmail.com",
+    "smtp_port": 587
+
+}
