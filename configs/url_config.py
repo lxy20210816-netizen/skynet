966f7cee1ffa992be923b98f095fc7f7ca6d0a92
lxy20210816 <lxy20210816@gmail.com>
2025-09-08 22:05:18 +0900

    中心整理下，以n8n为核心整理该项目

--- configs/url_config.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-#coding:utf-8
-
-yomiuri_url_config = {
-    "ranking": "https://www.yomiuri.co.jp/ranking/"
-}
--- sources/yfinance_source.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#coding:utf-8
-
-import yfinance as yf
-
-qqq = yf.Ticker("QQQ")
-pe_ratio = qqq.info.get("trailingPE", "N/A")
-print(f"QQQ 市盈率：{pe_ratio}")
